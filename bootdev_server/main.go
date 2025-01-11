1a8601adb267dd457cd72656f4f4a052c76b4b0d
viodid <david.yunta.aller@gmail.com>
2025-01-11 20:14:49 +0100

    server ex1

--- bootdev_server/main.go
+++ bootdev_server/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"errors"
 	"net/http"
-	"time"
 )
 
 func main() {
-	client := &http.Client{
-		Timeout: 30 * time.Second,
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
-
+	err := server.ListenAndServe()
+	if err != nil {
+		if !errors.Is(http.ErrServerClosed, err) {
+			panic(err)
+		}
+	}
 }
