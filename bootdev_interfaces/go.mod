8abadeb5336c0dbe4071fbc332c470de9ff8228a
viodid <david.yunta.aller@gmail.com>
2025-01-28 17:10:04 +0100

    interfaces

---
+++ bootdev_interfaces/go.mod
@@ -0,0 +1,3 @@
+module bootdev_interfaces
+
+go 1.23.4
---
+++ bootdev_interfaces/main.go
@@ -0,0 +1,33 @@
+package main
+
+type employee interface {
+	getName() string
+	getSalary() int
+}
+
+type contractor struct {
+	name         string
+	hourlyPay    int
+	hoursPerYear int
+}
+
+func (c contractor) getSalary() int {
+	return c.hoursPerYear * c.hourlyPay
+}
+
+func (c contractor) getName() string {
+	return c.name
+}
+
+type fullTime struct {
+	name   string
+	salary int
+}
+
+func (ft fullTime) getSalary() int {
+	return ft.salary
+}
+
+func (ft fullTime) getName() string {
+	return ft.name
+}
