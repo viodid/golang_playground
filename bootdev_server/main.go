dba20d6bfa01154cd4bc23486f763cad8cf7f3b4
viodid <david.yunta.aller@gmail.com>
2025-01-30 18:58:05 +0100

    header and responses

--- bootdev_server/main.go
+++ bootdev_server/main.go
@@ -10,6 +10,16 @@ func main() {
 	handler := http.FileServer(http.Dir('.'))
 	handler = http.StripPrefix("/app", handler)
 	mux.Handle("/app/", handler)
+	mux.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
+		request.Header = map[string][]string{
+			"Content-Type": {"text/plain", "charset=utf-8"},
+		}
+		writer.WriteHeader(200)
+		_, err := writer.Write([]byte("hey"))
+		if err != nil {
+			return
+		}
+	})
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
