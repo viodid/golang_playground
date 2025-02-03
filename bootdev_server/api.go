f43876cbc742f6d9db589bb43be7d24aaadbf3d9
viodid <david.yunta.aller@gmail.com>
2025-02-03 18:14:16 +0100

    fix boken handler chain middleware

--- bootdev_server/api.go
+++ bootdev_server/api.go
@@ -11,17 +11,18 @@ type apiConfig struct {
 }
 
 func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	// TODO: middleware does not activate or doesn't update fileServerHits
-	c.fileServerHits.Store(c.fileServerHits.Add(15))
-	return next
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (c *apiConfig) handler(writer http.ResponseWriter, request *http.Request) {
-	request.Header = map[string][]string{
+func (c *apiConfig) handler(w http.ResponseWriter, r *http.Request) {
+	r.Header = map[string][]string{
 		"Content-Type": {"text/plain", "charset=utf-8"},
 	}
 	body := fmt.Sprintf("Hits: %d\n", c.fileServerHits.Load())
-	_, err := writer.Write([]byte(body))
+	_, err := w.Write([]byte(body))
 	if err != nil {
 		return
 	}
--- bootdev_server/server.go
+++ bootdev_server/server.go
@@ -6,7 +6,7 @@ import (
 )
 
 func server() {
-	api := apiConfig{}
+	api := &apiConfig{}
 
 	mux := http.NewServeMux()
 
