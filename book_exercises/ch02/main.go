9fd31ae64438cfc31eb041f5108f223fcef2ea02
viodid <david.yunta.aller@gmail.com>
2025-05-09 14:23:40 +0200

    completed ch02

--- book_exercises/ch02/main.go
+++ book_exercises/ch02/main.go
@@ -5,11 +5,12 @@ import "fmt"
 func main() {
 	ex01()
 	ex02()
+	ex03()
 }
 
 func ex01() {
 	i := 20
-	var f float32 = float32(i)
+	var f = float32(i)
 	fmt.Println(i, f)
 }
 
@@ -18,4 +19,11 @@ func ex02() {
 	i := value
 	var f float32 = value
 	fmt.Println(i, f)
+}
+
+func ex03() {
+	var b byte = 1<<8 - 1
+	var smallI int32 = 1<<16 - 1
+	var bigI uint64 = 1<<64 - 1
+	fmt.Println(b, smallI, bigI)
 }
