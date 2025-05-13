46b793ec81ed2505e243d3def9159d22f71d16ca
viodid <david.yunta.aller@gmail.com>
2025-05-13 16:28:11 +0200

    ch03 ex03

--- book_exercises/ch03/main.go
+++ book_exercises/ch03/main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	ex01()
 	ex02()
+	ex03()
 }
 
 func ex01() {
@@ -30,4 +31,28 @@ func ex02() {
 	mb := []byte(message)
 	fmt.Println(rune(message[3]), len(message))
 	fmt.Printf("%v\n%v\n", mr, mb)
+}
+
+type Employee struct {
+	firstName string
+	lastName  string
+	id        uint
+}
+
+func ex03() {
+	i1 := Employee{
+		"Ana",
+		"Bohuelo",
+		1,
+	}
+	i2 := Employee{
+		firstName: "Ana",
+		lastName:  "Bohuelo",
+		id:        1,
+	}
+	var i3 Employee
+	i3.firstName = "Ana"
+	i3.lastName = "Bohuelo"
+	i3.id = 1
+	fmt.Println(i1, i2, i3)
 }
