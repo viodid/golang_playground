7921fbb8472c15238245c8adfddc1292644ab8d8
viodid <david.yunta.aller@gmail.com>
2025-05-21 13:41:32 +0200

    ch07 - ex02 wip test

--- book_exercises/ch07/main.go
+++ book_exercises/ch07/main.go
@@ -1,11 +1,33 @@
 package main
 
+import "fmt"
+
 type Team struct {
-	teamName    string
+	name        string
 	playerNames []string
 }
 
 type League struct {
-	Teams []Team
-	Wins  map[int]Team
+	Teams []string
+	Wins  map[string]int
+}
+
+func (l *League) MatchResult(team1 string, score1 int, team2 string, score2 int) error {
+	if score1 > score2 {
+		l.Wins[team1] += 1
+	} else if score2 > score1 {
+		l.Wins[team2] += 1
+	}
+	return nil
+}
+
+func main() {
+	t1 := Team{"t1Telecom", []string{"bob", "alice"}}
+	t2 := Team{"g2", []string{"foo", "bar"}}
+	l := League{[]string{t1.name, t2.name}, map[string]int{}}
+	err := l.MatchResult(t1.name, 42, t2.name, 13)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(l)
 }
