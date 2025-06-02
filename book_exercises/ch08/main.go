b9698f68eb46baa87814636392759d326bc58be5
viodid <david.yunta.aller@gmail.com>
2025-06-02 15:32:20 +0200

    ch08 - ex00

--- book_exercises/ch08/main.go
+++ book_exercises/ch08/main.go
@@ -5,13 +5,18 @@ import (
 )
 
 type IntOrFloat interface {
-	~int | ~float64
+	uint8 | uint16 | uint32 | uint64 |
+		int8 | int16 | int32 | int64 |
+		float32 | float64
 }
 
-func double[T IntOrFloat](n T) any {
+func double[T IntOrFloat](n T) T {
 	return n * 2
 }
 
 func main() {
-	fmt.Println(double(2.3))
+	var x float32 = 2.3
+	var y int8 = 42
+	fmt.Println(double(x))
+	fmt.Println(double(y))
 }
