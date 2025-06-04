762391102ace6a68ba96d579e3b2d218ddd53f63
viodid <david.yunta.aller@gmail.com>
2025-06-04 20:25:49 +0200

    ex02- printValue generic fun

--- book_exercises/ch08/main.go
+++ book_exercises/ch08/main.go
@@ -16,15 +16,27 @@ func double[T IntOrFloat](n T) T {
 }
 
 // == EX02 ==
-type Printabe[T fmt.Stringer] interface {
+type Printable interface {
 	~uint8 | ~uint16 | ~uint32 | ~uint64 |
 		~int8 | ~int16 | ~int32 | ~int64 |
 		~float32 | ~float64
+	fmt.Stringer
+}
+
+func printValue[P Printable](p P) {
+	fmt.Println(p)
+}
+
+type myFloat float32
+
+func (mf myFloat) String() string {
+	return fmt.Sprintf("%f", mf)
 }
 
 func main() {
 	var x float32 = 2.3
 	var y int8 = 42
-	fmt.Println(double(x))
-	fmt.Println(double(y))
+	fmt.Println(double(x), double(y))
+	var z myFloat = 42.42
+	printValue(z)
 }
