e2737e2b347c64edc2bc3b01a1095489a538b67b
David Yunta <davidy@ext.inditex.com>
2025-06-14 13:15:11 +0200

    ex01 - ex test - sentinel error def

--- book_exercises/ch09/main.go
+++ book_exercises/ch09/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// EX01 - Create a sentinel error to represent an invalid ID.
+// In `main`, use `errors.Is` to check for the sentinel error,
+// and pirnt a message when it is found.
+var ErrInvalidId error = errors.New("Invalid Id")
+
+func main() {
+	const x int = 42
+	fmt.Printf("hey there")
+}
